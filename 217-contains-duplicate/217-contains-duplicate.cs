97aad9bfe3d3aae6ff4706c088fa21ef5f7e669c
Mohamed Ali <94767811+moaligithub@users.noreply.github.com>
2022-08-03 10:01:23 -0700

    Time: 3897 ms (5.00%), Space: 50.4 MB (22.01%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cs
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -1,13 +1,9 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         for (int i = 0; i < nums.Length; i++)
-        {
-            List<int> list = nums.ToList();
-            list.Remove(nums[i]);
-            
-            if (list.Contains(nums[i]))
-                return true;
-        }
+            for (int j = 0; j < nums.Length; j++)
+                if (nums[i] == nums[j] && i != j)
+                    return true;
         
         return false;
     }
